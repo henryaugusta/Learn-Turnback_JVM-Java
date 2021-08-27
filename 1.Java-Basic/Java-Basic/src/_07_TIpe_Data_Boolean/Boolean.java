ba74ece7a82ae829ad08a2fd3972bd2e20599c2b
henryaugusta <henryaugusta4@gmail.com>
2021-08-27 10:43:12 +0700

    boolean

---
+++ 1.Java-Basic/Java-Basic/src/_07_TIpe_Data_Boolean/Boolean.java
@@ -0,0 +1,11 @@
+package _07_TIpe_Data_Boolean;
+
+public class Boolean {
+    public static void main(String[] args) {
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println(benar);
+        System.out.println(salah);
+    }
+}
